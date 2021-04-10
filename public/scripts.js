f2c672054a4260a860be45ea83b80ce0cb90cbe6
casey-manion <78034730+casey-manion@users.noreply.github.com>
2021-04-09 22:00:23 -0400

    UPDATE: completed random 10 selection of meals

--- public/scripts.js
+++ public/scripts.js
@@ -10,13 +10,13 @@ async function dataHandler() {
   if (request.ok) {
     console.log('endpoint fetched');
   } else {
-    alert('HTTP-Error: ' + request.status);
+    alert(`HTTP-Error: ${request.status}`);
   }
 
   // successful request, create Object for data
   const diningHalls = await request.json();
   const diningArr = diningHalls.data;
-  console.log('diningArr: ' , diningArr);
+  console.log('diningArr: ', diningArr);
 
   // iterate through data array, adding new row for each hall
   // using inner HTML
@@ -30,9 +30,15 @@ async function dataHandler() {
   });
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 async function getMeals() {
   console.log('enter getMeals');
-  const diningRequest = await fetch('/api/meals/');
+  const diningRequest = await fetch('/api/wholeMeal');
   const diningData = await diningRequest.json();
   return diningData;
 }
@@ -40,9 +46,17 @@ async function getMeals() {
 async function windowActions() {
   console.log('enter windowActions');
   // Lab 7 function
-  const meals = await getMeals();
-  console.table(meals);
-  
+  const results = await getMeals();
+  const meals = results.data;
+
+  const mealArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const selectedMeals = mealArr.map((elem) => {
+    const random = getRandomIntInclusive(0, meals.length - 1);
+    return meals[random];
+  });
+
+  console.table(selectedMeals);
+
   // Lab 6 function
   await dataHandler();
 }
