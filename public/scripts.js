800414678c73820a1cd285f0f6c74d950e2c652e
casey-manion <78034730+casey-manion@users.noreply.github.com>
2021-04-09 22:35:51 -0400

    UPDATE: added basic canvasjs stacked bar chart example to be edited w/ meal + macro info

--- public/scripts.js
+++ public/scripts.js
@@ -30,6 +30,7 @@ async function dataHandler() {
   });
 }
 
+// random int function for whole meal selection
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -56,6 +57,119 @@ async function windowActions() {
   });
 
   console.table(selectedMeals);
+
+  // build stacked bar chart
+  const chart = new CanvasJS.Chart('chartContainer', {
+    animationEnabled: true,
+    title: {
+      text: 'Evening Sales in a Restaurant'
+    },
+    axisX: {
+      valueFormatString: 'DDD'
+    },
+    axisY: {
+      prefix: '$'
+    },
+    toolTip: {
+      shared: true
+    },
+    legend: {
+      cursor: 'pointer',
+      itemclick: toggleDataSeries
+    },
+    data: [{
+      type: 'stackedBar',
+      name: 'Meals',
+      showInLegend: 'true',
+      xValueFormatString: 'DD, MMM',
+      yValueFormatString: '$#,##0',
+      dataPoints: [
+        { x: new Date(2017, 0, 30), y: 56 },
+        { x: new Date(2017, 0, 31), y: 45 },
+        { x: new Date(2017, 1, 1), y: 71 },
+        { x: new Date(2017, 1, 2), y: 41 },
+        { x: new Date(2017, 1, 3), y: 60 },
+        { x: new Date(2017, 1, 4), y: 75 },
+        { x: new Date(2017, 1, 5), y: 98 }
+      ]
+    },
+    {
+      type: 'stackedBar',
+      name: 'Snacks',
+      showInLegend: 'true',
+      xValueFormatString: 'DD, MMM',
+      yValueFormatString: '$#,##0',
+      dataPoints: [
+        { x: new Date(2017, 0, 30), y: 86 },
+        { x: new Date(2017, 0, 31), y: 95 },
+        { x: new Date(2017, 1, 1), y: 71 },
+        { x: new Date(2017, 1, 2), y: 58 },
+        { x: new Date(2017, 1, 3), y: 60 },
+        { x: new Date(2017, 1, 4), y: 65 },
+        { x: new Date(2017, 1, 5), y: 89 }
+      ]
+    },
+    {
+      type: 'stackedBar',
+      name: 'Drinks',
+      showInLegend: 'true',
+      xValueFormatString: 'DD, MMM',
+      yValueFormatString: '$#,##0',
+      dataPoints: [
+        { x: new Date(2017, 0, 30), y: 48 },
+        { x: new Date(2017, 0, 31), y: 45 },
+        { x: new Date(2017, 1, 1), y: 41 },
+        { x: new Date(2017, 1, 2), y: 55 },
+        { x: new Date(2017, 1, 3), y: 80 },
+        { x: new Date(2017, 1, 4), y: 85 },
+        { x: new Date(2017, 1, 5), y: 83 }
+      ]
+    },
+    {
+      type: 'stackedBar',
+      name: 'Dessert',
+      showInLegend: 'true',
+      xValueFormatString: 'DD, MMM',
+      yValueFormatString: '$#,##0',
+      dataPoints: [
+        { x: new Date(2017, 0, 30), y: 61 },
+        { x: new Date(2017, 0, 31), y: 55 },
+        { x: new Date(2017, 1, 1), y: 61 },
+        { x: new Date(2017, 1, 2), y: 75 },
+        { x: new Date(2017, 1, 3), y: 80 },
+        { x: new Date(2017, 1, 4), y: 85 },
+        { x: new Date(2017, 1, 5), y: 105 }
+      ]
+    },
+    {
+      type: 'stackedBar',
+      name: 'Takeaway',
+      showInLegend: 'true',
+      xValueFormatString: 'DD, MMM',
+      yValueFormatString: '$#,##0',
+      dataPoints: [
+        { x: new Date(2017, 0, 30), y: 52 },
+        { x: new Date(2017, 0, 31), y: 55 },
+        { x: new Date(2017, 1, 1), y: 20 },
+        { x: new Date(2017, 1, 2), y: 35 },
+        { x: new Date(2017, 1, 3), y: 30 },
+        { x: new Date(2017, 1, 4), y: 45 },
+        { x: new Date(2017, 1, 5), y: 25 }
+      ]
+    }]
+  });
+
+  chart.render();
+
+  // toggle data series for stacked bar chart
+  function toggleDataSeries(e) {
+    if (typeof (e.dataSeries.visible) === 'undefined' || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    chart.render();
+  }
 
   // Lab 6 function
   await dataHandler();
